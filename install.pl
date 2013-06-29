977532e03b5805a514e27ba9cf184cecee372bd8
Leo Lapworth <leo@cuckoo.org>
2013-06-29 21:23:09 +0100

    Throw some stuff in here and we'll see what happens

---
+++ install.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use JSON;
+use Path::Class;
+use File::Copy;
+
+my $d = decode_json( file('osx.json')->slurp);
+
+while( my ($from, $to) = each %{$d}) {
+	warn "F: $from T: $to";
+}
